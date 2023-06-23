c7d39ad681ff1f3845339f574bab9deac55af8cc
xmzheng <zhengxiaoming@gmail.com>
2023-06-23 10:21:07 -0700

    add precompiles

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { extendConfig, extendEnvironment } from "hardhat/config";
+import { TASK_NODE_GET_PROVIDER } from "hardhat/builtin-tasks/task-names";
+import { extendConfig, extendEnvironment, subtask } from "hardhat/config";
 import { lazyObject } from "hardhat/plugins";
-import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
+import {
+  EthereumProvider,
+  HardhatConfig,
+  HardhatUserConfig,
+} from "hardhat/types";
 import path from "path";
 
 import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
+import { precompile01 } from "./precompiles/01-random-bytes";
+import { precompile02 } from "./precompiles/02-x25519-key-derivation";
+import { precompile03 } from "./precompiles/03-deoxysii-seal";
+import { precompile04 } from "./precompiles/04-deoxysii-open";
+import { precompile05 } from "./precompiles/05-keypair-generate";
 // This import is needed to let the TypeScript compiler know that it should include your type
 // extensions in your npm package's types file.
 import "./type-extensions";
@@ -47,3 +57,44 @@ extendEnvironment((hre) => {
   // needed.
   hre.example = lazyObject(() => new ExampleHardhatRuntimeEnvironmentField());
 });
+
+subtask(TASK_NODE_GET_PROVIDER).setAction(
+  async (
+    args: {
+      forkBlockNumber?: number;
+      forkUrl?: string;
+    },
+    { artifacts, config, network, userConfig },
+    runSuper
+  ): Promise<EthereumProvider> => {
+    const Precompiles = await import(
+      "@nomicfoundation/ethereumjs-evm/dist/precompiles"
+    );
+    const getActivePrecompiles = Precompiles.getActivePrecompiles;
+    Precompiles.getActivePrecompiles = (...args1) => {
+      const activePrecompiles = getActivePrecompiles(...args1);
+      activePrecompiles.set(
+        "0100000000000000000000000000000000000001",
+        precompile01
+      );
+      activePrecompiles.set(
+        "0100000000000000000000000000000000000002",
+        precompile02
+      );
+      activePrecompiles.set(
+        "0100000000000000000000000000000000000003",
+        precompile03
+      );
+      activePrecompiles.set(
+        "0100000000000000000000000000000000000004",
+        precompile04
+      );
+      activePrecompiles.set(
+        "0100000000000000000000000000000000000005",
+        precompile05
+      );
+      return activePrecompiles;
+    };
+    return runSuper();
+  }
+);
